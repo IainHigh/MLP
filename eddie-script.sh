94456035682a8a1b76bd36f67cc6601fd3726710
marina <s1929142@ed.ac.uk>
2024-03-19 15:30:30 +0000

    pushing script to eddie

--- eddie-script.sh
+++ eddie-script.sh
@@ -1,13 +1,13 @@
 # !/bin/sh
 # Grid Engine options (lines prefixed with #$)
-#$ -N bert-1
+#$ -N random_forest_bert
 # runtime limit of 48 hrs
 #$ -l h_rt=48:00:00
 # set working directory to the directory of the MLP
 #$ -wd /exports/eddie/scratch/s1929142/MLP
 # set the output and error stream to the output directory:
-#$ -o /exports/eddie/scratch/s1929142/MLP/output
-#$ -e /exports/eddie/scratch/s1929142/MLP/output
+#$ -o /exports/eddie/scratch/s1929142/MLP/output/random_forest_bert_output.txt
+#$ -e /exports/eddie/scratch/s1929142/MLP/output/random_forest_bert_error.txt
 # request 80 GB system RAM per GPU
 #$ -l h_vmem=80G
 #  These options are:
